bc38bd933181da3955213ec31e687e16a6a41950
MohamedOsama011 <mohamed.osama.mo493@gmail.com>
2026-05-08 19:13:17 +0300

    Add Attendance Services

---
+++ GymSystem.BLL/Services/Interfaces/IAttendanceService.cs
@@ -0,0 +1,28 @@
+﻿using GymSystem.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.BLL.Services.Interfaces
+{
+    public interface IAttendanceService
+    {
+        Task<IEnumerable<AttendanceRecordDto>> GetTodayAsync(); 
+        Task<IEnumerable<AttendanceRecordDto>> GetByMemberAsync(int memberId); 
+        Task<IEnumerable<AttendanceRecordDto>> GetFilteredAsync(AttendanceFilterDto filter); 
+        Task<CheckInDto> GetCheckInDataAsync(); 
+        Task<CheckInResultDto> CheckInAsync(int memberId); 
+        Task<CheckInResultDto> CheckOutAsync(int attendanceId); 
+        Task<AttendanceStatsDto> GetStatsAsync(int? memberId = null); 
+    }
+    public class AttendanceStatsDto
+    {
+        public int TotalCheckIns { get; set; }
+        public int ActiveMembers { get; set; } // لسه موجودين
+        public double AverageDurationMinutes { get; set; }
+        public DateTime? BusiestDay { get; set; }
+        public int PeakHour { get; set; }
+    }
+}
