d2ad8e885b28a59dd8eab21dae838332b2b9d69d
MohamedOsama011 <mohamed.osama.mo493@gmail.com>
2026-05-08 03:30:41 +0300

    Add GymClass DTOs

---
+++ GymSystem.Models/DTOs/GymClassListDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.Models.DTOs
+{
+    public class GymClassListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TrainerName { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Capacity { get; set; }
+        public int EnrolledCount { get; set; }
+    }
+}
